01614efec98b517ff6b3586ede0aa4a79a328ca6
JeelanPro <133474801+JeelanPro@users.noreply.github.com>
2023-06-27 21:00:51 -0600

    Update RootTerminal.sh

--- RootTerminal.sh
+++ RootTerminal.sh
@@ -1,20 +1,47 @@
-
-if [ -d "./sys" ]
+if [ -d "./RootOS"]
 then
-./dist/proot -S . /bin/bash
+  cd RootOS/
+  if [ -d "./sys" ]
+  then
+    ./dist/proot -S . /bin/bash
+  else
+  clear
+  rm RootOS/
+  echo "Please Run This Code:"
+  echo "\"wget https://raw.githubusercontent.com/JeelanPro/Files/main/RootTerminal.sh && bash RootTerminal.sh\""
+  fi
+fi
 else
+clear
+echo "Do you Have mkdir, wget and unzip if not press \"ctrl + C\""
+echo "Chacking mkdir, wget and unzip..."
+sleep 2s
+clear
+wget --help
+unzip --help
+mkdir --help
 clear
 echo "-----------------------------------"
 echo "---- Root - by Jeelan Muhammad ----"
 echo "-----------------------------------"
+echo "Folder Creating..."
+sleep 1s
+mkdir RootOS
+cd RootOS/
+clear
+echo "-----------------------------------"
+echo "---- Root - by Jeelan Muhammad ----"
+echo "-----------------------------------"
+echo "Folder Created"
 echo "Installing Debian..."
-sleep 2s
+sleep 1s
 clear
 wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
 clear
 echo "-----------------------------------"
 echo "---- Root - by Jeelan Muhammad ----"
 echo "-----------------------------------"
+echo "Folder Created"
 echo "Installed Debian"
 echo "Setting Debian..."
 unzip akuh.zip
